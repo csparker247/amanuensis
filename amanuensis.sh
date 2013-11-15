c0b9e4350a50a05ec31aa670d08d6c023fb97608
Seth Parker <csparker247@gmail.com>
2013-11-15 16:40:19 -0500

    Diarization/segmentation metadata via LIUM.

--- amanuensis.sh
+++ amanuensis.sh
@@ -38,22 +38,38 @@ fi
 # Process each argument
 for (( i=1; i<=${args}; i++ )); do
 	index=$(expr $i - 1)
-	file="${filelist[$index]}"
-	echo "Processing $(basename "$file")..."
-	
-	tempdir="$(dirname "$file")/aman_temp"
+	filepath="${filelist[$index]}"
+	filename=$(basename "$filepath" | sed 's/\(.*\)\..*/\1/')
+	echo "Processing $filename..."
+	tempdir="$(dirname "$filepath")/aman_temp"
 	if [[ ! -d "$tempdir" ]]; then
 		mkdir "$tempdir"
 	else
-		echo Emptying temp folder...
+		echo Temp directory found. Emptying temp directory...
 		rm -rf "$tempdir"
 		mkdir "$tempdir"
 	fi
-	ffmpeg -i "$file" -vn -c:a flac -b:v 16000 -map 0:1 -f segment -segment_time 14 "$tempdir"/temp_%03d.flac
+	
+	echo Segmenting audio stream...
+	ffmpeg -i "$filepath" -vn -ar 16000 -ac 1 -map a:0 "$tempdir"/temp.wav 2> /dev/null
+	. lium_seg.sh
+	
+	# Read from "$tempdir"/temp.spl.3.seg
+
+	ffmpeg -i "$filepath" -vn -c:a flac -ar 16000 -map a:0 -f segment -segment_time 15 "$tempdir"/temp_%03d.flac 2> /dev/null
+	
+	# Create output transcript file
+	line="Amanuensis Transcription - $filename"
+	echo "$line" > "$(dirname "$filepath")/${filename}_Transcript.txt"
+	line=${#line}
+	printf '=%.0s' $(seq 1 $line) >> "$(dirname "$filepath")/${filename}_Transcript.txt"
+	echo >> "$(dirname "$filepath")/${filename}_Transcript.txt"
+	
+	# Send each segment for transcription
+	for segment in "$tempdir"/*.flac; do
+		wget -q -U "Mozilla/5.0" --post-file "$segment" --header "Content-Type: audio/x-flac; rate=16000" -O - "https://www.google.com/speech-api/v1/recognize?lang=en-us&client=chromium" | cut -d\" -f12 >> "$(dirname "$filepath")/${filename}_Transcript.txt"
+		echo >> "$(dirname "$filepath")/${filename}_Transcript.txt"
+	done
 done
 
 exit 0
-
-#ffmpeg -y -i - -ar 16000 -acodec flac file.flac
-#wget -q -U "Mozilla/5.0" --post-file file.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v1/recognize?lang=en-us&client=chromium" | cut -d\" -f12
-#rm file.flac
